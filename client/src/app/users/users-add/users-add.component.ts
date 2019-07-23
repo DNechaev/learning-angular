7157ba910c6d4f3877201557438fcf628ccb98f0
dim.nech@gmail.com <dim.nech@gmail.com>
2019-07-23 18:03:44 +0600

    Client: Added roles field in user creation form.

--- client/src/app/users/users-add/users-add.component.ts
+++ client/src/app/users/users-add/users-add.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormArray, ValidatorFn} from '@angular/forms';
 
 import { AuthenticationService } from '../../shared/services/authentication.service';
 import { LoaderIndicatorService } from '../../shared/services/loader-indicator.service';
@@ -9,6 +9,7 @@ import { User } from '../../shared/models/user.model';
 import { Router } from '@angular/router';
 import {SearchService} from '../../shared/services/search.service';
 import {Subscription} from 'rxjs';
+import {ToastService} from '../../shared/services/toast.service';
 
 
 @Component({
@@ -24,7 +25,11 @@ export class UsersAddComponent implements OnInit, OnDestroy {
   isLoading    = false;
 
   userForm: FormGroup;
-  error;
+  roles = [
+    { id: 1, name: Role.ADMIN },
+    { id: 2, name: Role.MANGER },
+    { id: 3, name: Role.USER },
+  ];
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -32,6 +37,7 @@ export class UsersAddComponent implements OnInit, OnDestroy {
     private router: Router,
     private loaderIndicatorService: LoaderIndicatorService,
     private searchService: SearchService,
+    private toastService: ToastService,
   ) {}
 
   ngOnInit() {
@@ -59,22 +65,50 @@ export class UsersAddComponent implements OnInit, OnDestroy {
       name: new FormControl(null, [Validators.required, Validators.minLength(4)]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(4)]),
+      roles: new FormArray([], this.minSelectedCheckboxes(1))
     });
+
+    this.addCheckboxes();
 
   }
 
   ngOnDestroy() {
-    console.log('[UsersAddComponent] ----ngOnDestroy----');
     this.subscriptions.map((subscription) => {
       subscription.unsubscribe();
     });
     this.subscriptions = null;
   }
 
+  private minSelectedCheckboxes(min = 1) {
+    const validator: ValidatorFn = (formArray: FormArray) => {
+      const totalSelected = formArray.controls
+        .map(control => control.value)
+        .reduce((prev, next) => next ? prev + next : prev, 0);
+      return totalSelected >= min ? null : { required: true };
+    };
+    return validator;
+  }
+
+  private addCheckboxes() {
+    this.roles.map((o, i) => {
+      const control = new FormControl(i === 2); // if first item set to true, else false
+      (this.userForm.controls.roles as FormArray).push(control);
+    });
+  }
+
+  private getSelectedRoleIds() {
+    const selectedRoleIds = this.userForm.value.roles
+      .map((v, i) => v ? this.roles[i].id : null)
+      .filter(v => v !== null);
+    return selectedRoleIds;
+  }
+
   onSubmit(user: User) {
+    user.roles = this.getSelectedRoleIds();
     console.log('User: ', user);
     this.usersService.createUser(user).subscribe(
       (data) => {
+        this.toastService.success('User created!');
         this.router.navigate(['/users']);
       },
       error => {
