9e2c86d19a6a84a98b317181ae6c0f2600e1b893
dim.nech@gmail.com <dim.nech@gmail.com>
2019-08-06 13:38:44 +0600

    Client: refactoring resolve-pre-fetching-component-data

---
+++ client/src/app/users/users-resolver.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { of, EMPTY } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { take, mergeMap } from 'rxjs/operators';
+
+import { User } from '../shared/models';
+import { UsersRoutesPath } from './users.routing';
+import { UsersService } from './users.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersResolverService implements Resolve<User> {
+
+  constructor(
+    private usersService: UsersService,
+    private router: Router
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<User> | Observable<never> {
+    const id = route.paramMap.get('id');
+
+    return this.usersService.getUserById(+id).pipe(
+      take(1),
+      mergeMap(user => {
+        if (user) {
+          return of(user);
+        } else { // id not found
+          this.router.navigate([UsersRoutesPath.PATH_TO_LIST]);
+          return EMPTY;
+        }
+      })
+    );
+  }
+
+}
